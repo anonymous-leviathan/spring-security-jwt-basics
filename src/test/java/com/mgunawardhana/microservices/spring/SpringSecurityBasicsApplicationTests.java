642d1af46d90d27b033412b0534c5903646fa073
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-06-21 10:47:20 +0530

    initializing project

---
+++ src/test/java/com/mgunawardhana/microservices/spring/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgunawardhana.microservices.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
